ca93cd6dc16c027002322c9b8a3c216852669821
Leandro Holanda <leandropholanda@Outlook.com>
2025-02-05 17:14:03 -0300

    adicionado StatusBar com style inverted para que seja possível visualizar a barra de notificações do celular

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,13 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
     return (
-        <Stack>
-            <Stack.Screen name={'index'} options={{ title: 'Campo minado', headerShown: false }} />
-        </Stack>
+        <>
+            <Stack>
+                <Stack.Screen name={'index'} options={{ title: 'Campo minado', headerShown: false }} />
+            </Stack>
+            <StatusBar style="inverted" />
+        </>
     )
 }
